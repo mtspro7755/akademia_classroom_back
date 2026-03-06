f3cc191290a72eac540981cde03eb9bfe2e67a29
mtspro7755 <mtspro7755@gmail.com>
2026-03-06 16:42:59 +0000

    config scrumble v2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewApiDocs', function ($user = null) {
+            // Accessible a tous puis restreindre à certaines adresses IP ou emails plus tard
+            return true;
+        });
     }
+
 }
