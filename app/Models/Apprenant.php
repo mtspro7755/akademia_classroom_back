65b2c56e97d0b150c878810271b2f946ecf04096
mtspro7755 <mtspro7755@gmail.com>
2026-03-20 21:13:58 +0000

    ajout des relationships dans le model Aprenant

--- app/Models/Apprenant.php
+++ app/Models/Apprenant.php
@@ -35,6 +35,33 @@ class Apprenant extends Authenticatable implements JWTSubject
         return $this->belongsTo(Profil::class);
     }
 
+    public function cohortes()
+    {
+        return $this->belongsToMany(Cohorte::class, 'apprenant_cohorte');
+    }
+
+    public function penalites()
+    {
+        return $this->hasMany(Penalite::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function quetes()
+    {
+        return $this->belongsToMany(Quete::class, 'apprenant_quete')
+            ->withPivot(['statut','dureeEffective','dateSoumission'])
+            ->withTimestamps();
+    }
+
+    public function livrables()
+    {
+        return $this->hasMany(Livrable::class);
+    }
+
     public function getJWTIdentifier()
     {
         return $this->getKey();
