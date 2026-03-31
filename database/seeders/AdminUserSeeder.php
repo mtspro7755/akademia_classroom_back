70f17c92509145a06e4a5e5c519bad346d432d6d
mtspro7755 <mtspro7755@gmail.com>
2026-03-31 11:41:36 +0000

    Correction du deploiement côté filament

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Apprenant;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Apprenant::updateOrCreate(
+            ['email' => 'admin@terangacode.com'],
+            [
+                'nomComplet' => 'admin',
+                'phone' => '770000000',
+                'pseudo' => 'admin',
+                'password' => Hash::make('admin'),
+                'role' => 'admin',
+                'statutCompte' => true,
+            ]
+        );
+    }
+}
