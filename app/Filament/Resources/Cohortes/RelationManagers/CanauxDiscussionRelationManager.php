1d2b9ccdde4d2e12ab1ade4bc1b5815a7578d6ae
mtspro7755 <mtspro7755@gmail.com>
2026-05-18 11:58:50 +0000

    fix: erreur lors de la création d'un canal de discussion dans cohorte

--- app/Filament/Resources/Cohortes/RelationManagers/CanauxDiscussionRelationManager.php
+++ app/Filament/Resources/Cohortes/RelationManagers/CanauxDiscussionRelationManager.php
@@ -5,13 +5,29 @@ namespace App\Filament\Resources\Cohortes\RelationManagers;
 use Filament\Actions\CreateAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\DeleteAction;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
 use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 
 class CanauxDiscussionRelationManager extends RelationManager
 {
     protected static string $relationship = 'canalDeDiscussion';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('titre')
+                    ->required()
+                    ->maxLength(255),
+                Textarea::make('description')
+                    ->required()
+                    ->rows(3),
+            ]);
+    }
 
     public function table(Table $table): Table
     {
